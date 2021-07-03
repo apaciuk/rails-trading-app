f1d78b43cfad662148c2be63e9f061e1d4fdbca7
Paul Anthony McGowan <xhostcom@gmail.com>
2021-07-03 12:26:37 +0100

    Messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,11 +5,6 @@ module ApplicationHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "border-radius-50")
     end
-    def markdown_tohtml(text)
-        Kramdown::Document.new(text, input: "GFM").to_html
-    end
-
-   
   def trade_author(trade)
     user_signed_in? && current_user.id == trade.user_id
   end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,9 +2,9 @@ class Message < ApplicationRecord
     belongs_to :conversation
     belongs_to :user
 
-    vaildates_presence_of :body, :converstion_id, :user_id
+    validates_presence_of :body, :conversation_id, :user_id
       
     def message_time
-       create_at.strftime("%m/%d/%y at %l:%M %p")
+       created_at.strftime("%m/%d/%y at %l:%M %p")
     end
 end
